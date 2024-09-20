b64d139c686cf51d7871fbb85a54877061b70df7
plough <126720664+walkingplough@users.noreply.github.com>
2024-09-20 21:30:02 +0200

    Added image carousel for food page.

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+        "./node_modules/flowbite/**/*.js",
+        // Add other paths to your HTML or JS files here
+    ],
+    plugins: [
+        require('flowbite/plugin')
+    ]
+};
+document.body.style.overflow = 'hidden';
